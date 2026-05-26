fa4478fc1bdb8434a24400d2097499fcbc75963d
이은서 <hg231049@gamil.com>
2026-05-26 15:10:16 +0900

    배포시도

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,6 @@ const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
 export default defineConfig({
-  base: "/Dles_Portfolio/",   // ⭐️ 이거 추가 (repo 이름)
   plugins: [react()],
   resolve: {
     alias: {
