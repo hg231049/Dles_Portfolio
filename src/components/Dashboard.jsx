2b276507670d299204dd36de3b4a9d4831845771
이은서 <hg231049@gamil.com>
2026-05-26 17:19:50 +0900

    대시보드 페이지 성능 최적화 노션링크 버튼 추가

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -67,12 +67,25 @@ const Dashboard = () => {
         
         {/* 상단 타이틀 영역 */}
         <div className="py-20 text-center">
-          <h1 className="text-2xl lg:text-5xl font-black mb-4">
+          <h1 className="text-2xl lg:text-5xl font-black mb-10">
             기술적 SEO 및 웹 성능 최적화
           </h1>
           <p className="text-gray-500 text-sm lg:text-lg">
-            데이터 기반의 사용자 경험 개선 및 검색 엔진 노출 극대화 프로젝트
+            SEO 및 성능 개선 과정에서 <br />
+            실제 측정 데이터를 기반으로<br />
+            문제 분석과 개선 과정을 기록했습니다.<br />
+           * Google Search Console / Lighthouse 기반 *
           </p>
+          <div className="mt-10">
+            <a
+            href="https://www.notion.so/36c5cb715070809db794dbfd884c4406?source=copy_link"
+            target="_blank"
+            rel="noreferrer"
+            className="inline-flex items-center gap-2 px-5 py-3 rounded-full border border-black/10 bg-white/100 hover:bg-white/20 transition"
+          >
+            상세 개선 리포트 보기
+          </a>
+          </div>
         </div>
 
         {/* 🌟 3. 핵심 성과 지표 대시보드 + 미니 라인 차트 통합 섹션 */}
