9bf7f8ca1bf845ac0c1da842d2457f45589109d8
이은서 <hg231049@gamil.com>
2026-05-26 15:16:39 +0900

    파일명 변경 배포시도

--- src/components/Competencies.jsx
+++ src/components/Competencies.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { competencies } from "@/data/Competencies";
+import { competencies } from "@/data/competencies";
 
 export const Competencies = () => {
   return (
