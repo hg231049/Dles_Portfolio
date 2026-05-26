631383951f367483e3d093d1096c46e076bf9cce
이은서 <hg231049@gamil.com>
2026-05-26 14:56:49 +0900

    배포

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
-import { fileURLToPath } from 'url' // 추가
+import { fileURLToPath } from 'url'
 
-// ESM 환경에서 __dirname 기능을 대신함
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
 export default defineConfig({
+  base: "/Dles_Portfolio/",   // ⭐️ 이거 추가 (repo 이름)
   plugins: [react()],
   resolve: {
     alias: {
